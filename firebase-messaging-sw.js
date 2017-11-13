5ab456660a24a53bb9bfc21fa5dac3c1042297cd
DIPush <push@dialoginsight.com>
2017-11-13 10:32:42 -0500

    update sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,6 +6,7 @@
  });
 
  const messaging = firebase.messaging();
+ const applicationKey = '3238:vvz7WQEc3UPHmb7gf9CzHMlbJny261Ee';
 
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
@@ -65,9 +66,75 @@ function showNotification(payload)
       notificationOptions.data['action1_url'] = payload.data.Action2_URL;
   }
 
+  if(payload.data.OFSYSReceptionID)
+  {
+     var data = {
+      "ApplicationId": applicationKey,
+      "PushId": payload.data.OFSYSReceptionID,
+      "dtReception": new Date()
+    };
+
+    corsAjax({
+      "url" : 'https://lightspeed.dev.osfys.com/webservices/ofc4/mobile.ashx?method=Mobile_TrackPushReception', 
+      "data" : JSON.stringify(data), 
+      "success" : function(responseText)
+      {
+      }, 
+      "error" : function(status, responseText)
+      {
+      }
+    });
+  }
+
   return self.registration.showNotification(notificationTitle,
       notificationOptions);
 }
+
+  function corsAjax(options)
+  {
+    try
+    {
+      var ajaxRequest = new XMLHttpRequest();
+      var gotStdCors = ('withCredentials' in ajaxRequest);
+      var gotXDomain = (window.XDomainRequest != null);
+    
+      if (gotXDomain && !gotStdCors)
+      {
+        // IE 8 et 9 n'implémentent pas le standard, mais ont l'extension XDomainRequest qui fait un peu la même chose 
+        var xdr = new XDomainRequest();
+        xdr.open("POST", options.url);
+        //xdr.onprogress = function () { };
+        xdr.ontimeout = function () { options.error(408, 'timeout'); };
+        xdr.onerror = function () { options.error(500, 'error'); };
+        xdr.onload = function() { options.success(xdr.responseText); }
+        setTimeout(function () { xdr.send(options.data); }, 0);
+      }
+      else
+      {
+        ajaxRequest.open('POST', options.url, true);
+        ajaxRequest.setRequestHeader("Content-type", "text/plain");
+        ajaxRequest.onreadystatechange = function()
+        {
+          //console.log('onreadystatechange',ajaxRequest.readyState);
+          if (ajaxRequest.readyState == 4 /* complete */)
+          {
+            if (ajaxRequest.status == 200)
+              options.success(ajaxRequest.responseText);
+            else
+              options.error(ajaxRequest.status, ajaxRequest.responseText);
+          }
+        };
+        ajaxRequest.send(options.data); 
+      }
+    }
+    catch (e)
+    {
+      options.error(500, 'exception ' + e);
+      if (window.console)
+        console.error(e);
+    }
+  }
+
 
 self.addEventListener('notificationclick', function(event) {
   console.log(event);
