7bcfde6452ecc8a28627608574497c20b86782c2
jfcadrin <jfcadrin@dialoginsight.com>
2018-03-02 16:05:52 -0500

    change

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -72,8 +72,7 @@ function showNotification(payload)
         var date = new Date();
         var data = {
             "ApplicationId": applicationKey,
-            "PushId": payload.data.OFSYSReceptionID,
-            "dtReception": '' + date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + 'z'
+            "PushId": payload.data.OFSYSReceptionID
         };
 
         corsAjax({
