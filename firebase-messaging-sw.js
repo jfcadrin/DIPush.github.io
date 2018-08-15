937477e87788e5c4853590de4b0f451ec0e72c52
jfcadrin <jfcadrin@dialoginsight.com>
2018-08-15 15:21:49 -0400

    correction

--- firebase-messaging-sw.js
+++
@@ -1,3 +0,0 @@
- importScripts('https://www.gstatic.com/firebasejs/4.5.0/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/4.5.0/firebase-messaging.js');
- importScripts('https://lightspeed.dev.ofsys.com/js/service.js');
