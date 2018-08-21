53b61e3e5e8153a118c06c3799c3bef7c2e005d0
jfcadrin <jfcadrin@dialoginsight.com>
2018-08-21 16:45:45 -0400

    correction

--- dialoginsight-webpush-sw.js
+++ dialoginsight-webpush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/377/4891/XysvZ3rIJPek7NKIaqazWtWqYEG20NKE/dialoginsight-webpush-sw.js?' + new Date().getTime());
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/377/4891/XysvZ3rIJPek7NKIaqazWtWqYEG20NKE/dialoginsight-webpush-sw.js');
