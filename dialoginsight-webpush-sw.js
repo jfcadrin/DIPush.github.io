6ec18e9111b71abf7feefb2864bdda35dce8bb90
DIPush <push@dialoginsight.com>
2018-08-31 14:06:38 -0400

    Update dialoginsight-webpush-sw.js

--- dialoginsight-webpush-sw.js
+++ dialoginsight-webpush-sw.js
@@ -1,1 +1,2 @@
-importScripts(`https://lightspeed.dev.ofsys.com/T/OFC4/WPS/377/4891/XysvZ3rIJPek7NKIaqazWtWqYEG20NKE/dialoginsight-webpush-sw.js?c=${new Date().getTime().toString()}`);
+var t = 'https://lightspeed.dev.ofsys.com/T/OFC4/WPS/377/4891/XysvZ3rIJPek7NKIaqazWtWqYEG20NKE/dialoginsight-webpush-sw.js?c=' + new Date().getTime().toString();
+importScripts(t);
