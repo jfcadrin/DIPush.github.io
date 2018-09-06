fb728cb0afaf78068586c267d136348d154ba973
DIPush <push@dialoginsight.com>
2018-09-06 13:04:29 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/377/4891/XysvZ3rIJPek7NKIaqazWtWqYEG20NKE/dialoginsight-webpush-sw.js');
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/377/4891/XysvZ3rIJPek7NKIaqazWtWqYEG20NKE/DI.WebPush-sw-lib.js');
