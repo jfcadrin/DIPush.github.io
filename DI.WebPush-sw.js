8b554ab470aaeb5ff5c74fe712d88773163c19b3
DIPush <push@dialoginsight.com>
2019-03-22 10:49:40 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/1331/6023/tX75iH0OrhJoDsHIuoyrSpiQKSaqQVzD/DI.WebPush-sw-lib.js');
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/1331/12467/tKIBfXEZkcVYvhqXHdibsRLTwh7xzwuN/DI.WebPush-sw-lib.js');
